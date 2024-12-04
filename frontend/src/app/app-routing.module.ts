73f2d564211e5459d07e90f51c4824076c72dbdb
Badian-AD <Faun350@gmail.com>
2024-12-05 01:32:06 +0300

    Kartinki v istorii

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,38 +7,35 @@ import { AppLayoutComponent } from './layout/app-layout/app-layout-component';
 import { AUTH_LAYOUT_ROUTES } from './routes/auth-layout.routes';
 import { APP_LAYOUT_ROUTES } from './routes/app-layout.routes';
 
-
 const appRoutes: Routes = [
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full',
-    },
-    {
-        path: '',
-        component: AppLayoutComponent,
-        children: APP_LAYOUT_ROUTES
-    },
-    {
-        path: '',
-        component: AuthLayoutComponent,
-        children: AUTH_LAYOUT_ROUTES
-    }
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: APP_LAYOUT_ROUTES
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: AUTH_LAYOUT_ROUTES
+  }
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(appRoutes, {
-            preloadingStrategy: PreloadAllModules,
-            anchorScrolling: 'enabled',
-            scrollPositionRestoration: 'enabled',
-            relativeLinkResolution: 'legacy'
-        })
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [
+    RouterModule.forRoot(appRoutes, {
+      preloadingStrategy: PreloadAllModules,
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled',
+      relativeLinkResolution: 'legacy'
+    })
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
