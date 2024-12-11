caca6b2b8f68600a2d86c0b141a3506065c56b2f
SmallShorty <smallshorty1012@gmail.com>
2024-12-12 02:59:12 +0300

    Добавлены кнопки для редактирования страницы: (#22)
    Реализовано перенаправление в админ-панель для управления контентом.

--- frontend/src/app/views/uits/public/scientific-activities/achievements/achievement-list/achievement-list.component.ts
+++ frontend/src/app/views/uits/public/scientific-activities/achievements/achievement-list/achievement-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+
 import { AchievementService } from '../achievement.service';
+import { AuthService } from '@app/shared/services/auth.service'; // Импорт AuthService
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-achievement-list',
@@ -11,18 +13,28 @@ export class AchievementListComponent implements OnInit {
   achievements: any[] = [];
   isLoading = true; // Индикатор загрузки
   errorMessage: string | null = null; // Сообщение об ошибке
+  isAdmin = false; // Флаг для проверки прав администратора
 
   constructor(
     private achievementService: AchievementService,
+    private authService: AuthService, // Внедрение AuthService
     private router: Router,
-    private cdr: ChangeDetectorRef // Добавлено ChangeDetectorRef
+    private cdr: ChangeDetectorRef // ChangeDetectorRef для ручного обновления
   ) {}
 
   ngOnInit(): void {
+    this.checkAdminRights(); // Проверка прав администратора
     this.fetchAchievements();
   }
 
-  fetchAchievements() {
+  checkAdminRights(): void {
+    this.authService.canEdit().subscribe((canEdit: boolean) => {
+      this.isAdmin = canEdit; // Обновляем флаг
+      this.cdr.detectChanges(); // Обновляем представление вручную
+    });
+  }
+
+  fetchAchievements(): void {
     this.achievementService.getAchievements().subscribe({
       next: (data) => {
         this.achievements = data.results;
@@ -37,16 +49,20 @@ export class AchievementListComponent implements OnInit {
     });
   }
 
-  reloadPage() {
+  reloadPage(): void {
     this.isLoading = true;
     this.errorMessage = null;
     this.fetchAchievements();
   }
 
-  onSelectAchievement(achievement: any) {
+  onSelectAchievement(achievement: any): void {
     this.router.navigate([
       '/scientific-activities/achievements',
       achievement.id,
     ]);
   }
+
+redirectToAdminPanel(): void {
+    window.open('http://127.0.0.1:8000/admin/achievements/achievement/', '_blank');
+}
 }
