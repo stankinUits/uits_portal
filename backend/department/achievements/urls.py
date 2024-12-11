988bac40ba36e86a971c0f1d069f8b1840c63539
dsnvel <vdovenko-main@yandex.ru>
2024-12-12 02:32:05 +0300

    dsnvel: fix code of make, compose, bug of front: changes for deploy

--- backend/department/achievements/urls.py
+++ backend/department/achievements/urls.py
@@ -1,10 +1,10 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import AchievementAPIViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AchievementAPIViewSet
 
-# router = DefaultRouter()
-# router.register('', AchievementAPIViewSet)
+router = DefaultRouter()
+router.register('', AchievementAPIViewSet)
 
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
+urlpatterns = [
+     path('', include(router.urls)),
+]
--- frontend/src/app/layout/components/header-navbar/header-navbar.component.ts
+++ frontend/src/app/layout/components/header-navbar/header-navbar.component.ts
@@ -4,7 +4,7 @@ import { navConfiguration } from '@app/configs/nav.config';
 import { NavMenuColor } from '@app/shared/types/app-config.interface';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import { ChangeDetectorRef } from '@angular/core'; // Импортируем ChangeDetectorRef
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
     selector: 'header-navbar',
@@ -14,37 +14,44 @@ import { ChangeDetectorRef } from '@angular/core'; // Импортируем Cha
       '[class.nav-menu-light]': 'color === "light"',
       '[class.nav-menu-dark]': 'color === "dark"'
     }
-  })
-  export class HeaderNavbarComponent implements OnInit {
-  
-    menu: NavMenu[] = [];
+})
+export class HeaderNavbarComponent implements OnInit {
+
+    menu: NavMenu[] = []; // Меню для отображения
     @Input() color: NavMenuColor = 'light';
-    isProfilePage: boolean = false; // Добавляем переменную
-  
+    isProfilePage: boolean = false; // Флаг для определения корпоративной страницы
+
     constructor(
       private router: Router,
       private cdr: ChangeDetectorRef
     ) { }
-  
+
     ngOnInit(): void {
-      this.router.events.pipe(
-          filter(event => event instanceof NavigationEnd)
-      ).subscribe(() => {
-          console.log('Обновляем видимость меню после изменения маршрута: ', this.router.url);
-          this.updateMenuVisibility();
-      });
+        // Инициализация меню при загрузке компонента
+        this.updateMenuVisibility();
+
+        // Подписка на изменения маршрута для обновления видимости меню
+        this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd)
+        ).subscribe(() => {
+            console.log('Обновляем видимость меню после изменения маршрута: ', this.router.url);
+            this.updateMenuVisibility();
+        });
     }
-  
+
     updateMenuVisibility(): void {
-      this.isProfilePage = this.router.url.startsWith('/corp');
-      if (this.isProfilePage) {
-          console.log('Корпоративный портал: скрываем меню');
-          this.menu = [];
-      } else {
-          console.log('Обычная страница: показываем меню');
-          this.menu = navConfiguration;
-      }
-      this.cdr.detectChanges(); // Обновляем представление
+        // Проверяем, находится ли текущий URL в разделе корпоративного портала
+        this.isProfilePage = this.router.url.startsWith('/corp');
+
+        if (this.isProfilePage) {
+            console.log('Корпоративный портал: скрываем меню');
+            this.menu = []; // Очистка меню
+        } else {
+            console.log('Обычная страница: показываем меню');
+            this.menu = navConfiguration; // Установка стандартной конфигурации меню
+        }
+
+        // Принудительное обновление представления
+        this.cdr.detectChanges();
     }
-  }
-  
+}
