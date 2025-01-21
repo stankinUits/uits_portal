8d6e8a3d85ad31c26a5c38752e68b82c0601018a
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-01-21 15:48:16 +0300

    fix urls department

--- backend/department/urls.py
+++ backend/department/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('news/', include('department.news.urls')),
     path('achievements/', include('department.achievements.urls')),
     path('employee/', include('department.employee.urls')),
-    path('postgraduate/', include('department.employee.postgraduate.urls'))
+    path('postgraduate/', include('department.employee.postgraduate.urls')),
     path('scientific_publications/', include('department.scientific_publications.urls')),
 ]
