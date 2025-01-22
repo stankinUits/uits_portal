95031fa4660e2e0efa48256a008603d3bdd7add9
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-01-23 01:27:25 +0300

    Requirement 1.3.1, 1.3.2, 1.3.3, 1.3.4 Updated teacher card section

---
+++ frontend/src/app/views/uits/public/about/employee/teachers/teacher/components/publications/publications-teacher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publications',
+  templateUrl: './publications-teacher.component.html',
+  styleUrls: ['./publications-teacher.component.css']
+})
+export class PublicationsTeacherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
