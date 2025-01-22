cb164d2bd94f27e49c641bc9e90705339238dcf8
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-01-23 01:26:32 +0300

    Requirement 1.3.1, 1.3.2, 1.3.3, 1.3.4 Updated teacher card section

---
+++ frontend/src/app/views/uits/public/about/employee/teachers/teacher/components/achievements/achievements-teacher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-achievements',
+  templateUrl: './achievements-teacher.component.html',
+  styleUrls: ['./achievements-teacher.component.css']
+})
+export class AchievementsTeacherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/views/uits/public/about/employee/teachers/teacher/components/general/general.component.ts
@@ -0,0 +1,80 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {EmployeeService} from '@app/views/uits/public/about/employee/employee.service';
+import {BehaviorSubject} from 'rxjs';
+import {IEmployee} from '@app/shared/types/models/employee';
+import {AVATAR_DEFAULT_URL} from '@app/configs/app.config';
+import {TeacherDegree, TeacherRank} from '@app/views/uits/public/about/employee/teachers/teachers.models';
+
+@Component({
+  selector: 'app-general',
+  templateUrl: './general.component.html',
+  styleUrls: ['./general.component.css']
+})
+export class GeneralComponent implements OnInit {
+  teacherID: number;
+  teacher$: BehaviorSubject<IEmployee>;
+
+  constructor(private route: ActivatedRoute,
+              private employeeService: EmployeeService,) {
+    this.teacher$ = new BehaviorSubject(null);
+  }
+
+  get avatar() {
+    const teacher = this.teacher$.getValue();
+    return (!teacher.avatar) ? AVATAR_DEFAULT_URL : teacher.avatar;
+  }
+
+  get fullName() {
+    const teacher = this.teacher$.getValue();
+    return `${teacher.last_name} ${teacher.first_name} ${teacher.patronymic}`;
+  }
+
+  get shortName() {
+    const teacher = this.teacher$.getValue();
+    return `${teacher.last_name} ${teacher.first_name[0]}. ${teacher.patronymic ? teacher.patronymic[0] + '.' : ''}`;
+  }
+
+  get position() {
+    return this.teacher$.getValue().position;
+  }
+
+  get degree() {
+    return TeacherDegree[this.teacher$.getValue().degree];
+  }
+
+  get rank() {
+    return TeacherRank[this.teacher$.getValue().rank];
+  }
+
+  get professional_experience() {
+    return this.teacher$.getValue().professional_experience;
+  }
+
+  get experience() {
+    return this.teacher$.getValue().experience;
+  }
+
+  get education() {
+    return this.teacher$.getValue().education.split('\n');
+  }
+
+  get qualification() {
+    return this.teacher$.getValue().qualification;
+  }
+
+  get bio(){
+    return this.teacher$.getValue().bio;
+  }
+
+  ngOnInit(): void {
+    this.route.parent?.paramMap.subscribe(params => {
+      this.teacherID = +params.get('id')!;
+      console.log('Para3432432ms:', this.teacherID);
+      this.employeeService.retrieveTeacher(this.teacherID).subscribe(teacher => {
+        this.teacher$.next(teacher);
+        console.log('dfgfdgf', teacher);
+      });
+    });
+  }
+}
