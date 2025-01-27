433dfad0794738dd5b1308a03bba5295da031726
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-01-27 17:23:06 +0300

    linter

--- frontend/src/app/views/uits/public/educational-activities/educational-activities.module.ts
+++ frontend/src/app/views/uits/public/educational-activities/educational-activities.module.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EducationalActivitiesRoutingModule } from './educational-activities-routing.module';
 import { BachelorEduPlansComponent } from './bachelor/bachelor-edu-plans/bachelor-edu-plans.component';
@@ -9,24 +9,21 @@ import { MasterGraduateComponent } from './master/master-graduate/master-graduat
 import { MasterPracticesComponent } from './master/master-practices/master-practices.component';
 import { ScheduleSummaryComponent } from './schedule/schedule-summary/schedule-summary.component';
 import { LayoutModule } from '@app/layout/layout.module';
-import { NgSelectModule } from '@ng-select/ng-select'; // Если нужно для фильтров
-import { DateFnsModule } from 'ngx-date-fns'; // Для работы с датами
-import { ModalModule } from 'ngx-bootstrap/modal'; // Модальные окна, если они используются
-import {adapterFactory} from "angular-calendar/date-adapters/date-fns";
+import { NgSelectModule } from '@ng-select/ng-select';
+import { DateFnsModule } from 'ngx-date-fns';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 import {
-  CalendarDateFormatter,
   CalendarModule,
-  CalendarNativeDateFormatter,
   DateAdapter,
-  DateFormatterParams
-} from "angular-calendar";
+} from 'angular-calendar';
 import { AboutModule } from '../about/about.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ExamScheduleComponent } from './schedule/exam-schedule/exam-schedule.component';
 
 @NgModule({
   declarations: [
-    ScheduleSummaryComponent, // Компонент для календаря
+    ScheduleSummaryComponent,
     BachelorEduPlansComponent,
     BachelorGraduateComponent,
     BachelorPracticesComponent,
@@ -35,20 +32,20 @@ import { ExamScheduleComponent } from './schedule/exam-schedule/exam-schedule.co
     MasterPracticesComponent,
     ExamScheduleComponent
   ],
-  imports: [
-    CommonModule,
-    EducationalActivitiesRoutingModule,
-    LayoutModule,
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory
-  }),
-    NgSelectModule, // Если нужно для выбора
-    DateFnsModule, // Для работы с датами
-    ModalModule.forRoot(), // Если используются модальные окна
-    AboutModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ]
+    imports: [
+        CommonModule,
+        EducationalActivitiesRoutingModule,
+        LayoutModule,
+        CalendarModule.forRoot({
+            provide: DateAdapter,
+            useFactory: adapterFactory
+        }),
+        NgSelectModule,
+        DateFnsModule,
+        ModalModule.forRoot(),
+        AboutModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ]
 })
 export class EducationalActivitiesModule { }
