e3371aa05f908152767b64331e187c6d64e6a740
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-08-31 17:39:59 +0300

    feat: new styles for history of department

--- frontend/src/app/views/uits/public/about/history-of-department/history-of-department.component.ts
+++ frontend/src/app/views/uits/public/about/history-of-department/history-of-department.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-history-of-department',
@@ -6,11 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history-of-department.component.scss']
 })
 export class HistoryOfDepartmentComponent implements OnInit {
+  scrollPercent = 0;
+
+  @ViewChild('progressBar') progressBar: ElementRef;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    const currentScrollY = window.scrollY; // сколько пикселей пролистали сверху
+    const pageHeight = document.documentElement.scrollHeight; // вся высота страницы
+    const windowHeight = window.innerHeight; // видимая часть окна
+
+    const scrollableHeight = pageHeight - windowHeight;
+    this.scrollPercent = (currentScrollY / scrollableHeight) * 100;
+    this.progressBar.nativeElement.style.width = `${this.scrollPercent}%`;
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
-    // Инициализация компонента
-  }
-
+  ngOnInit(): void {}
 }
